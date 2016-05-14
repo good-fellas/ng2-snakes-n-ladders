522ce8bb2a46ed128f5e44032229ce5dfb6c0479
Brij Kishor <kishorsharma7@gmail.com>
2016-05-14 19:30:29 +0530

    updated home page

--- app/home/home.component.ts
+++ app/home/home.component.ts
@@ -4,9 +4,30 @@ import {Component} from "@angular/core";
 @Component({
     selector: 'home',
     template: `
-        This is home page
-    `
+        <div class="container col-lg-11">
+            <div class="row">
+                <div>This is Snake and ladder game:</div>
+                <div class="col-md-5">
+                    This is Place for rules:
+                    <ul>
+                        <li>RUle 1</li>
+                        <li>Rule 2</li>
+                        <li>Rule 3</li>
+                        <li>Rule 4</li>
+                    </ul>
+                </div>
+                <div class="col-md-3">
+                    this section is for input name
+                    <form class="form">
+                        <input type="text" class="form-control"  placeholder="name">
+                        <input type="text" class="form-control" placeholder="name">
+                    </form>
+                </div>
+            </div>
+        </div>
+            `
 })
 export class HomeComponent {
+
 
 }
