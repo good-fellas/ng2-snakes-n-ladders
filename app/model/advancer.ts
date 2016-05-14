f834795a5e6f57e01ddf43cebe0b0106502e59a3
Brij Kishor <kishorsharma7@gmail.com>
2016-05-14 19:34:34 +0530

    Removed duplicate declaration.

--- app/model/advancer.ts
+++ app/model/advancer.ts
@@ -1,8 +1,5 @@
 export class Advancer {
-    type:string
-    initialCellIndex:number
-    finalCellIndex:number
-    
+
     constructor(public type:string, public initialCellIndex:number, public finalCellIndex:number){
         
     }
