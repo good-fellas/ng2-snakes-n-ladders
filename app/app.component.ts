fd4f2e7a218d57ead653901b62a9e580ecc5652e
Brij Kishor <kishorsharma7@gmail.com>
2016-05-14 09:51:19 +0530

    Updated project for typescript.

---
+++ app/app.component.ts
@@ -0,0 +1,7 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'my-app',
+    template: '<h1>AngularAttack 2016 - Good fellas</h1><p>Your project runs!</p>'
+})
+export class AppComponent { }
